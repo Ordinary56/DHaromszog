10ace0c130efa3f17cd662eb4c38a80b0736aa67
tbalogh695@gmail.com <tbalogh695@gmail.com>
2022-11-13 11:26:37 +0100

    70% done

--- SOURCE/Script(PROTO).js
+++ SOURCE/Script(PROTO).js
@@ -1,27 +1,40 @@
-class ImgNode
-{
-    static next = null;
-    constructor(val) {this.val = val;}
+var buttonCount = 5
+var nav = document.getElementById("navigation")
+var selectedBtn = 0
+var pictures = ["food1.jpg","food2.jpg","food3.jpg","food2.jpg","food1.jpg"]
+var main = document.getElementById("Main")
+var bgPos = 2 
+function GenerateBTNs() {
+    let position = -((buttonCount-1)/2*200)
+    nav.innerHTML = '<button id="selected"></button>'
+    document.getElementById("selected").style.transform = 'translate('+(selectedBtn*145-2)+'%,22.5%)'
+    //Gombok legenerálása
+    for (let i = 0; i < buttonCount; i++) {
+        nav.innerHTML += '<button id="btn'+i+'" class="btn""></button>'
+        //Gombok közötti távolság beállítása
+        document.getElementById("btn"+i).style.transform = 'translate('+position+'%,50%)'
+        position += 200
+    }
 }
-
-N1 = new ImgNode(10)
-N2 = new ImgNode(1)
-N3= new ImgNode(2)
-N1.next = N2;
-N2.next =N3;
-N3.next = null;
-
-/*function Traverse( n) 
-{
-     temp = n
-     if(n != null) 
-     {
-        do{
-        console.log(temp.value);
-        temp = n.next
-        }
-        while(temp!=n)
-     }
+function UpdateImg() {
+    main.style.backgroundImage = 'url("Étterem/Desszertek/'+pictures[bgPos]+'")'
 }
-*/
-Traverse(N1);
+function MoveRight(){
+    if(selectedBtn != (buttonCount-1)/2){
+        selectedBtn = selectedBtn += 1
+        bgPos+=1
+        GenerateBTNs()
+        UpdateImg()
+    }
+    
+}
+function MoveLeft(){
+    if(selectedBtn != -(buttonCount-1)/2){
+        selectedBtn = selectedBtn -= 1
+        GenerateBTNs()
+        bgPos-=1
+        UpdateImg()
+    }
+}
+GenerateBTNs()
+UpdateImg()
